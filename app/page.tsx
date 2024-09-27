28527a03681aa9512e9ea6c4ca214aade3ed1e08
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-09-27 21:39:09 +0700

    feat: initial branch commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
-      landing page
-      <br />
-      <a href="/login">Login</a>
+      <h1 className="text-6xl text-center font-Rubik font-bold">Empower Your Finances,<br /> Simplify Your Decisions.</h1>
+      <h1 className="text-6xl text-center font-bold">Empower Your Finances,<br /> Simplify Your Decisions.</h1>
+      <p>Track, manage, and optimize your organization&apos;s finances effortlessly</p>
+      
+      <Link className={buttonVariants({variant:"default"})} href="#">Get Started</Link>
     </div>
   );
 }
