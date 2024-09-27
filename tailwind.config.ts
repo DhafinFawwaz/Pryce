1c480e2abdf08778cfb5b4392578768cfaa52f29
Rafly Hanggaraksa <ajarafly422@gmail.com>
2024-09-27 21:13:34 +0700

    feat: shadcn setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,76 +1,118 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      borderRadius: {
-        low: '8px',
-        medium: '16px',
-        high: '32px',
-      },
-      fontFamily: {
-          RubikBold: ['Rubik-Bold', 'sans-serif'],
-          RubikSemiBold: ['Rubik-SemiBold', 'sans-serif'],
-          RubikRegular: ['Rubik-Regular', 'sans-serif'],
-          Inter: ['Inter-Regular', 'sans-serif'],
-      },
-      fontSize: {
-        h1: ['30px', '120%'],
-        h2: ['20px', '120%'],
-        h3: ['20px', '120%'],
-        sh1: ['20px', '120%'],
-        sh2: ['20px', '120%'],
-        b1: ['20px', '120%'],
-        b2: ['13px', '120%'],
-        st: ['12px','120%'],
-      },
-      colors: {
-          black: '#000000',
-          muted: {
-            200: '#B3AFCA', //mute bg button
-            400: '#828181', //mute color button
-            700: '#5A5959', //mute bg button
-          },
-          bone: '#e8edfd', //background dashboard
-          purple: {
-            200: '#EAEAFB', //hover
-            600: '#988bee', //purple
-          },
-          orange: {
-            100: 'F8F1E4', // bg tag type (transaction)
-            400: '#FFB50A', //cashflow expense
-            800: '#FF5F00', //pie chart
-          },
-          white: {
-            100: '#FFFFFF', //white
-            200: '#FEFEFE', //filter dashboard bg
-            300: '#EDEDEE', //border filter dashboard bg
-          },
-          green: {
-            200: '#6EA47E', //BG tag type (transaction)
-            400: '#9AE66E', //pie chart
-            600: '#8DCE97', //cashflow income
-          },
-          pink: 'F10086', //pie chart
-          sky: {
-            400: '#3A86FF',
-            600: '#34B3F1',
-          }, //pie chart
-          gray: {
-            300: '#68717D', //light text
-            400: '#6B6B77', //icon settings
-            800: '#959292', // login icon
-            900: '#4A5260', //title card
-          },
-          navy: '#07071B', //primary
-      },
-    }
+  	extend: {
+  		borderRadius: {
+  			low: '8px',
+  			medium: '16px',
+  			high: '32px',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		fontFamily: {
+  			RubikBold: ['Rubik-Bold', 'sans-serif'],
+  			RubikSemiBold: ['Rubik-SemiBold', 'sans-serif'],
+  			RubikRegular: ['Rubik-Regular', 'sans-serif'],
+  			Inter: ['Inter-Regular', 'sans-serif']
+  		},
+  		fontSize: {
+  			h1: ['30px', '120%'],
+  			h2: ['20px', '120%'],
+  			h3: ['20px', '120%'],
+  			sh1: ['20px', '120%'],
+  			sh2: ['20px', '120%'],
+  			b1: ['20px', '120%'],
+  			b2: ['13px', '120%'],
+  			st: ['12px','120%']
+  		},
+  		colors: {
+  			black: '#000000',
+  			muted: {
+  				'200': '#B3AFCA',
+  				'400': '#828181',
+  				'700': '#5A5959',
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			bone: '#e8edfd',
+  			purple: {
+  				'200': '#EAEAFB',
+  				'600': '#988bee'
+  			},
+  			orange: {
+  				'100': 'F8F1E4',
+  				'400': '#FFB50A',
+  				'800': '#FF5F00'
+  			},
+  			white: {
+  				'100': '#FFFFFF',
+  				'200': '#FEFEFE',
+  				'300': '#EDEDEE'
+  			},
+  			green: {
+  				'200': '#6EA47E',
+  				'400': '#9AE66E',
+  				'600': '#8DCE97'
+  			},
+  			pink: 'F10086',
+  			sky: {
+  				'400': '#3A86FF',
+  				'600': '#34B3F1'
+  			},
+  			gray: {
+  				'300': '#68717D',
+  				'400': '#6B6B77',
+  				'800': '#959292',
+  				'900': '#4A5260'
+  			},
+  			navy: '#07071B',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
