1a5475e9380fed833db790a288f65ac5a03152c1
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-10-03 19:57:32 +0700

    feat: OCR Component

--- app/cameratest/ocr.tsx
+++ app/cameratest/ocr.tsx
@@ -4,8 +4,6 @@ import Webcam from "react-webcam";
 import { createWorker } from "tesseract.js";
 
 
-
-
 type WebcamProps = {
     getScreenshot: () => string;
 }
@@ -21,10 +19,15 @@ type OCRButton = {
     words: TextBox[];
 }
 
-export default function OCR() {
+type OCRProps = {
+    onConfirm: (imgSrc: string, totalMoney: number) => void;
+}
+
+export default function OCR({onConfirm}: OCRProps) {
     const webcamRef = useRef(null);
     const [imgSrc, setImgSrc] = useState<string>("");
     const [ocrButton, setOCRButton] = useState<OCRButton|null>(null);
+    const [isFront, setIsFront] = useState<boolean>(false);
 
     const inputRef = useRef(null);
 
@@ -32,6 +35,10 @@ export default function OCR() {
         setImgSrc("");
         setOCRButton(null);
     }, []);
+
+    function flip() {
+        setIsFront(!isFront);
+    }
 
     const capture = useCallback(async() => {
         if(webcamRef.current === null) return;
@@ -47,20 +54,31 @@ export default function OCR() {
 
         const worker = await createWorker('eng');
         await worker.setParameters({
-            textord_min_linesize: '3',
+            textord_min_linesize: '1',
         });
         const { data } = await worker.recognize(image);
 
         const extractedNumbers: TextBox[] = data.words
-            .filter(word => /(\d+)|\./.test(word.text)) // Only get words that are numbers
+            .filter(word => /^\d+(\.\d+)?$/.test(word.text)) // Only get words that are numbers
             .map((word, index) => ({
             text: word.text,
             bbox: word.bbox, // Bounding box coordinates (x0, y0, x1, y1)
             isSelected: false,
             index: index
         }));
-        extractedNumbers[extractedNumbers.length-1].isSelected = true;
+
+        // set the latest non 0 as true
+        for(let i = extractedNumbers.length-1; i >= 0; i--) {
+            if(extractedNumbers[i].text !== '0') {
+                extractedNumbers[i].isSelected = true;
+                break;
+            }
+        }
+
         setOCRButton({ width, height, words: extractedNumbers });
+
+        if(!inputRef.current) return;
+        (inputRef.current as HTMLInputElement).value = extractedNumbers[extractedNumbers.length-1].text;
 
     }, [webcamRef]);
 
@@ -85,21 +103,27 @@ export default function OCR() {
         setOCRButton({ width: ocrButton?.width || 0, height: ocrButton?.height || 0, words });
     }
 
-    return <>
+    function onConfirmClicked() {
+        if(!inputRef.current) return;
+        if(!onConfirm) return;
+        onConfirm(imgSrc, parseFloat((inputRef.current as HTMLInputElement).value));
+    }
+
+    return <div className="max-w-96">
 
 { !imgSrc ?
-<Webcam ref={webcamRef}/>
+<Webcam ref={webcamRef} videoConstraints={!isFront ? {facingMode:  "user"} : {facingMode: { exact: "environment" }}} className="rounded-2xl" />
 :
 <>
 <div className="relative inline-block">
     <img src={imgSrc} alt="" />
     { ocrButton?.words.map((word, index) => (<>
-        <button key={index} onClick={() => onOCRButtonClick(word)} className={`absolute bg-opacity-20 p-2  border-2 border-dashed rounded-md hover:scale-110 duration-150 ease-out-back hover:bg-opacity-50 active:bg-green-600 active:border-green-600 active:bg-opacity-20 ${!word.isSelected ? 'bg-indigo-600 border-indigo-600':'bg-green-600 border-green-600'}`} style={
+        <button key={index} onClick={() => onOCRButtonClick(word)} className={`absolute bg-opacity-20 border-2 border-dashed rounded-md hover:scale-110 duration-150 ease-out-back hover:bg-opacity-50 active:bg-green-600 active:border-green-600 active:bg-opacity-20 ${!word.isSelected ? 'bg-indigo-600 border-indigo-600':'bg-green-600 border-green-600'}`} style={
             {
-                left: `${word.bbox.x0/ocrButton.width*100}%`, 
+                left: `${word.bbox.x0/ocrButton.width*100 -1}%`, 
                 top: `${word.bbox.y0/ocrButton.height*100 -1}%`, 
-                width: `${(word.bbox.x1-word.bbox.x0)/ocrButton.width*100}%`, 
-                height: `${(word.bbox.y1-word.bbox.y0)/ocrButton.height*100}%`,
+                width: `${(word.bbox.x1-word.bbox.x0)/ocrButton.width*100 +2}%`, 
+                height: `${(word.bbox.y1-word.bbox.y0)/ocrButton.height*100 +2}%`,
             }
         }>
             {/* {word.text} */}
@@ -111,14 +135,33 @@ export default function OCR() {
 </>
 }
 
-<div className="grid gap-4">
+<div className="grid gap-4 mt-4">
     <div className="grid grid-cols-2 gap-4">
-        <button className="focus:ring-4 focus:ring-indigo-500 bg-indigo-600 rounded-xl font-bold text-slate-50 text-base hover:bg-indigo-700 hover:scale-102 dark:text-slate-50 w-full p-2 cursor-pointer disabled:bg-night-500 disabled:text-night-200 disabled:cursor-not-allowed tracking-wider" onClick={capture}>Capture</button>
-        <button className="focus:ring-4 focus:ring-indigo-500 bg-indigo-600 rounded-xl font-bold text-slate-50 text-base hover:bg-indigo-700 hover:scale-102 dark:text-slate-50 w-full p-2 cursor-pointer disabled:bg-night-500 disabled:text-night-200 disabled:cursor-not-allowed tracking-wider" onClick={restart}>Restart</button>
+
+        {!imgSrc ?
+        <button className="focus:ring-4 focus:ring-indigo-500 bg-indigo-600 rounded-xl font-bold text-slate-50 text-base hover:bg-indigo-700 hover:scale-102 dark:text-slate-50 w-full p-2 cursor-pointer disabled:bg-night-500 disabled:text-night-200 disabled:cursor-not-allowed tracking-wider flex justify-center" onClick={capture}>
+            <img className="z-10 p-1 pr-2" src="https://api.iconify.design/mdi/camera.svg?color=%23ffffff" alt="" />
+            Capture
+        </button>
+        :
+        <button className="focus:ring-4 focus:ring-indigo-500 bg-indigo-600 rounded-xl font-bold text-slate-50 text-base hover:bg-indigo-700 hover:scale-102 dark:text-slate-50 w-full p-2 cursor-pointer disabled:bg-night-500 disabled:text-night-200 disabled:cursor-not-allowed tracking-wider" onClick={restart}>Recapture</button>
+        }
+        
+        <button className="focus:ring-4 focus:ring-indigo-500 bg-indigo-600 rounded-xl font-bold text-slate-50 text-base hover:bg-indigo-700 hover:scale-102 dark:text-slate-50 w-full p-2 cursor-pointer disabled:bg-night-500 disabled:text-night-200 disabled:cursor-not-allowed tracking-wider flex justify-center" onClick={flip}>
+            <img className="z-10 p-1 pr-2" src="https://api.iconify.design/mingcute/cash-2-fill.svg?color=%23ffffff" alt="" />
+            Flip
+        </button>
     </div>
-    <input ref={inputRef} type="number" className="appearance-none sm:text-sm rounded-xl block w-full bg-night-600 border-2 placeholder-night-400 text-white duration-200 drop-shadow-sm ease-out-back h-10 ring-indigo-500 hover:ring-1 border-none pl-12 outline-none focus:ring-4" />
+
+    <div>Is this your number</div>
+    <div className="relative flex gap-4">
+        <img className="absolute z-10 p-3" src="https://api.iconify.design/mingcute/cash-2-fill.svg?color=%2354565c" alt="" />
+        <input ref={inputRef} placeholder="Expense" type="number" className="appearance-none sm:text-sm rounded-xl block w-full bg-night-600 border-2 placeholder-night-400 text-white duration-200 drop-shadow-sm ease-out-back h-10 ring-indigo-500 hover:ring-1 border-none pl-12 outline-none focus:ring-4" />
+        <button className="focus:ring-4 focus:ring-indigo-500 bg-indigo-600 rounded-xl font-bold text-slate-50 text-base hover:bg-indigo-700 hover:scale-102 dark:text-slate-50 p-2 cursor-pointer disabled:bg-night-500 disabled:text-night-200 disabled:cursor-not-allowed tracking-wider px-4" onClick={onConfirmClicked}>Confirm</button>
+        
+    </div>
 </div>
 
 
-</>
+</div>
 }
